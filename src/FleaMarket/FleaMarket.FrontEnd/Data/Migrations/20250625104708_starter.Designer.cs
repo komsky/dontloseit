4a97aa632767f2b242a5f7958de7cebffb604f28
Adrian Ilewicz <komsky@gmail.com>
2025-06-25 12:48:13 +0100

    db migration

--- src/FleaMarket/FleaMarket.FrontEnd/Data/Migrations/20250625104708_starter.Designer.cs
+++ src/FleaMarket/FleaMarket.FrontEnd/Data/Migrations/20250625104708_starter.Designer.cs
@@ -4,6 +4,7 @@ using FleaMarket.FrontEnd.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FleaMarket.FrontEnd.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250625104708_starter")]
+    partial class starter
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
