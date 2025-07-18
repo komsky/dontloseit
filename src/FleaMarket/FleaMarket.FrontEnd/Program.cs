e0a833a908141594d29a3645cecc96c92c4ab247
komsky <komsky@gmail.com>
2025-07-18 10:29:49 +0100

    build and exec fix

--- src/FleaMarket/FleaMarket.FrontEnd/Program.cs
+++ src/FleaMarket/FleaMarket.FrontEnd/Program.cs
@@ -11,8 +11,7 @@ namespace FleaMarket.FrontEnd
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
+            builder.Configuration.AddEnvironmentVariables();
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
